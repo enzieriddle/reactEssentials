c5b72aee666b2dc3540097397a916bc264ff36d7
Enzie Riddle <enzieriddle@Enzies-MacBook-Air.local>
2025-01-09 19:28:07 -0600

    components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx'
+import TabButton from './components/TabButton';
+
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -8,22 +16,23 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept {...CORE_CONCEPTS[0]} />
+          <CoreConcept {...CORE_CONCEPTS[1]} />
+          <CoreConcept {...CORE_CONCEPTS[2]} />
+          <CoreConcept {...CORE_CONCEPTS[3]} />
+        </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
-  )
-}
-
-function Header() {
-  return (
-    <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
   )
 }
 
