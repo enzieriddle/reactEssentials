f1293e18b8c1c0812a08ed370c29b5343d23eb95
Enzie Riddle <enzieriddle@Enzies-MacBook-Air.local>
2025-01-15 19:42:05 -0600

    Add setState functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import { useState } from 'react'
 import './App.css'
 
 import { CORE_CONCEPTS } from './data';
-import Header from './components/Header/Header.jsx';
+import { EXAMPLES } from './data.js';
 import CoreConcept from './components/CoreConcept.jsx'
+import Header from './components/Header/Header.jsx';
 import TabButton from './components/TabButton';
 
 import reactImg from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
   const [count, setCount] = useState(0)
 
   return (
@@ -28,8 +36,20 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
